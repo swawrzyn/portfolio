256ded8accfd14c61e6437141c563510493594aa
Stefan Wawrzyn <swawrzyn@gmail.com>
2021-06-28 17:36:06 +0800

    oops forgot to remove axios from nuxt config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,8 +40,6 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
